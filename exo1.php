df19b99ef17542e8e3f235c7700f60735c3b2a7b
celineMend <celine.mendy@unchk.edu.sn>
2024-02-20 15:27:46 +0000

    premiere exercice algorithme

---
+++ exo1.php
@@ -0,0 +1,11 @@
+<?php
+function produit_element_tableau($tableau) {
+    $produit =1;
+    foreach ($tableau as $element ){
+        $produit *= $element;
+    }
+    return $produit;
+}
+$tableau =array(1,2,3,4,5);
+$resultat= produit_element_tableau($tableau);
+echo "le produit des éléments du tableau est : " . $resultat;
